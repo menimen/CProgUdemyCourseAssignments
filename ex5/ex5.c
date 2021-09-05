9d6743ba37f2bec667a8bdb9a4565a8e63d6971b
menimen <menash9288@gmail.com>
2021-09-05 20:41:40 +0300

    finished seveal funcs in ex5:task 3

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -89,12 +89,69 @@ void setArr(int *arrptr, int *highest, int *lowest) {
     printResformission2(arrptr, highest, lowest);
 }
 void printResformission2(int *arrptr, int *highest, int *lowest) {
-    int i = 0;
-    double avg = 0, total = 0;
+    int i = 0, total = 0;
+    double avg = 0;
     while (i < MONTHS) {
         total += arrptr[i];
         i++;
     }
-    avg = (total / i);
-    printf("total is %f, avg is %f, highest is %d, lowest is %d\n", total, avg, *highest, *lowest);
+    avg = (total / (double)MONTHS);
+    printf("total is %d, avg is %f, highest is %d, lowest is %d\n", total, avg, *highest, *lowest);
 }
+//Write a program to find the similar elements in an array and find its occurrence. Display the array element, index of the element and its number of occurrence.
+void mission3Menu() {
+    char *buffer;
+    size_t siz = 255;
+    DataBase* db = NULL;
+    buffer = (char *)malloc(siz * sizeof(char));
+    getchar(); //dummy
+    printf("enter elements one by one seperated wiht spaces, press \"enter\" to finish: \n");
+    getline(&buffer, &siz, stdin);
+    db = initializeDB();
+    db = EnterNumbersToDB(db, buffer);
+    clearString(buffer);
+}
+void clearString(char *str) {
+    free(str);
+    str = NULL;
+}
+DataBase* EnterNumbersToDB(DataBase* db, char *str) {
+    char* token;
+    int i = strlen(str);
+    size_t count = 0;
+    int num = 0;
+    const char delim[2] = " ";
+    str[i-1] = '\0';
+    token = strtok(str, delim);
+   
+   /* walk through other tokens */
+   while( token != NULL ) {
+      num = atoi(token);
+      count++;
+      db->initialArr = initializeArr(&count, db->initialArr);
+      db -> initialArr[count] = num;
+      token = strtok(NULL, delim);
+   }
+   db->finalArr = initializeArr(&count, db->finalArr);
+   return db;
+}
+int isNumber(char *n) {
+  int i = strlen(n);
+  int isnum = (i>0);
+  while (i-- && isnum) {
+    if (!((n[i] >= '0' && n[i] <= '9') || (n[0] == '-') )) {
+      isnum = 0;
+    }
+  }
+  return isnum;
+}
+DataBase* initializeDB(){
+    DataBase *db = (DataBase*)malloc(sizeof(DataBase));
+    return db;
+}
+int* initializeArr(size_t *arrSize, int* arr) {
+    arr = (int*)realloc(arr, (*arrSize)*sizeof(int));
+    return arr;
+}
+
+
--- ex5/ex5.h
+++ ex5/ex5.h
@@ -1,8 +1,10 @@
 #ifndef ex5_H_   /* Include guard */
+#define  _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <ctype.h>
 #include <math.h>
 #include <string.h>
+#include<stdlib.h>
 #define ARR_SIZE 5
 #define MONTHS 12
 void mission1Menu();
@@ -16,4 +18,32 @@ void mult(int* num, int multWithNum);
 enum month{Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
 void setArr(int *arrptr, int *highest, int *lowest);
 void printResformission2(int *arrptr, int *highest, int *lowest);
+typedef struct Value{ 
+    short unsigned int occurences;
+    char indices[127] ; 
+} Value; 
+typedef struct{ 
+    int key;
+    Value val; 
+} Item;
+typedef struct{
+    // Contains an array of pointers
+    // to items
+    Item** items;
+    int size;
+    int count;
+}HashTable;
+typedef struct{
+    int *initialArr;
+    HashTable* table;
+    int *finalArr;
+}DataBase;
+Value getItemVal(HashTable* hash, int key);
+void addItem(HashTable* hash, Item* item);
+Item* create_item(int key, int index);
+DataBase* initializeDB();
+int isNumber(char *n);
+int* initializeArr(size_t *arrSize, int* arr);
+void clearString(char *str);
+DataBase* EnterNumbersToDB(DataBase* db, char *str);
 #endif // ex5_H_
